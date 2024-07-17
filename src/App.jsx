95f5af56174b59acabe9458f79461858adc6637b
Ankit <ankitmittal0814@gmail.com>
2024-07-17 22:29:06 +0530

    Update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import DashboardRoutes from "./components/DashboardRoutes";
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
-      <Route path="/dashboard/" element={<DashboardRoutes />} />
+      <Route path="/dashboard/*" element={<DashboardRoutes />} />
     </Routes>
   );
 }
