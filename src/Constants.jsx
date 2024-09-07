3f57e5ed849fe305205e2c658534fe472e6b3a49
Ankit Mittal <ankitmittal0814@gmail.com>
2024-09-07 23:54:55 +0530

    create Admin category edit and delete

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 const Constants = {
   API_URL: "http://15.207.165.187/api",
+  IMAGE_URL: "https://sriina-marketplace.s3.ap-south-1.amazonaws.com/",
 };
 export default Constants;
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,14 +6,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 // import formData from FormData();
-
 import Constants from "../Constants";
-const { API_URL } = Constants;
+
+const { API_URL, IMAGE_URL } = Constants;
 const Category = () => {
   const token = localStorage.getItem("token");
   const [categories, setCategories] = useState([]);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const [showPopup, setShowPopup] = useState(false);
+  const [categoryName, setCategoryName] = useState("");
+  const [duplicateCategory, setDuplicateCategory] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const [showEditPopup, setShowEditPopup] = useState(false);
+  const [editCategoryId, setEditCategoryId] = useState(null);
 
   const fetchCategory = async () => {
     setLoading(true);
@@ -45,39 +53,79 @@ const Category = () => {
   const handleSubCategory = (categoryId) => {
     navigate(`sub-category/${categoryId}`);
   };
+
+  //FIXME: Delete category
   const deleteCategory = async (categoryId) => {
     try {
       const response = await axios.post(
         `${API_URL}/category/delete`,
-        categoryId
+        { categoryId: categoryId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
-      console.log("response: ", response);
+      setCategories((prevCategories) =>
+        prevCategories.filter((category) => categoryId !== category._id)
+      );
+      // console.log("response: ", response);
     } catch (error) {
       console.log("Error: ", error);
     }
   };
-  const editCategory = async (categoryId) => {
+
+  //FIXME: edit category
+  const handleCloseEditPopup = () => {
+    setShowEditPopup(false);
+  };
+
+  const handleOpenEditPopup = (categoryId) => {
+    setEditCategoryId(categoryId);
+    setShowEditPopup(true);
+    setCategoryName("");
+    // setDuplicateCategory(false);
+  };
+  const handleEditCategory = async () => {
     try {
       const formData = new FormData();
-      formData.append("categoryId", categoryId);
+      formData.append("categoryId", editCategoryId);
       formData.append("name", categoryName);
       formData.append("image", selectedFile);
       const response = await axios.post(
         "http://15.207.165.187/api/category/add",
-        formData
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
-      console.log(response);
+      setCategories((prevCategories) =>
+        prevCategories.map((category) =>
+          category._id === editCategoryId
+            ? {
+                ...category,
+                name: categoryName,
+                image: selectedFile,
+              }
+            : category
+        )
+      );
+      // console.log("response: ", response);
+      setShowEditPopup(false);
     } catch (error) {
       console.log("Error: ", error);
+    }
+  };
+  const setEditOnEnter = (e) => {
+    if (e.key === "Enter") {
+      handleEditCategory();
     }
   };
 
   //FIXME: Add category code
 
-  const [showPopup, setShowPopup] = useState(false);
-  const [categoryName, setCategoryName] = useState("");
-  const [duplicateCategory, setDuplicateCategory] = useState(false);
-  const [selectedFile, setSelectedFile] = useState(null);
   const handleClosePopup = () => {
     setShowPopup(false);
   };
@@ -109,7 +157,7 @@ const Category = () => {
           maxBodyLength: Infinity,
         }
       );
-      console.log("Request: ", response.data.data);
+      // console.log("Request: ", response.data.data);
 
       const addedCategory = response.data.data;
       if (addedCategory) {
@@ -146,6 +194,52 @@ const Category = () => {
           Add Category
         </button>
       </div>
+
+      {showEditPopup && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-gray-300 p-6 rounded-md w-96">
+            <h2 className="text-xl font-bold mb-4 text-gray-700">
+              Edit Category
+            </h2>
+            <input
+              type="text"
+              value={categoryName}
+              onChange={handleInputChange}
+              onKeyDown={(e) => setEditOnEnter(e)}
+              placeholder="Enter Category"
+              className="border border-gray-500 p-2 rounded-md w-full mb-1 text-gray-800 bg-gray-200 outline-none"
+            />
+            <label
+              htmlFor="profile-picture"
+              className="cursor-pointer px-3 py-2 focus:outline-none mt-0 text-blue-600"
+            >
+              Upload Image
+            </label>
+            <input
+              id="profile-picture"
+              name="profile-picture"
+              type="file"
+              onChange={handleFileChange}
+              className="hidden"
+            />
+
+            <div className="mt-3 flex justify-between">
+              <button
+                onClick={handleEditCategory}
+                className="bg-blue-500 hover:bg-blue-700 text-white px-5 py-2 rounded-md mr-2"
+              >
+                {loading ? "Editing..." : "Edit"}
+              </button>
+              <button
+                onClick={handleCloseEditPopup}
+                className="bg-red-500 hover:bg-red-700 text-white px-4 py-2 rounded-md"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       {showPopup && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-gray-300 p-6 rounded-md w-96">
@@ -206,7 +300,7 @@ const Category = () => {
                 <tr className="text-left ">
                   <th className="py-2 text-xl px-4 border-b">Name</th>
                   <th className="py-2 text-xl border-b text-center w-96">
-                    Sub-Category
+                    Image
                   </th>
                   <th className="py-2 text-xl border-b w-28 text-center">
                     Edit
@@ -221,15 +315,27 @@ const Category = () => {
                   <tr
                     key={category._id}
                     className="text-lg hover:bg-gray-600 cursor-pointer"
-                    onClick={() => handleSubCategory(category._id)}
                   >
-                    <td className="py-2 px-4">{category.name}</td>
-                    <td className="py-2 text-center">
-                      {/* {category.productSubCategories.length} */}
+                    <td
+                      className="py-2 px-4"
+                      onClick={() => handleSubCategory(category._id)}
+                    >
+                      {category.name}
+                    </td>
+                    <td
+                      className="py-2 text-center"
+                      onClick={() => handleSubCategory(category._id)}
+                    >
+                      <div className="flex justify-center lg:justify-center ">
+                        <img
+                          src={`${IMAGE_URL}${category.image}`}
+                          className="w-10 h-10 mx-auto lg:mx-auto rounded-md mb-0"
+                        />
+                      </div>
                     </td>
                     <td
                       className="py-2 text-center hover:bg-blue-600"
-                      onClick={() => editCategory(category._id)}
+                      onClick={() => handleOpenEditPopup(category._id)}
                     >
                       <FontAwesomeIcon icon={faPenToSquare} />
                     </td>
